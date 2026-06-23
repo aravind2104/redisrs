737308207aaf21d374f685538eeeba001e003263
Aravind <aravindanil2101@gmail.com>
2026-06-23 19:13:16 +0530

    feat: CRUD commands and related store methods

--- src/commands.rs
+++ src/commands.rs
@@ -3,22 +3,81 @@ use crate::store::Store;
 use std::sync::Arc;
 
 pub fn execute(args: Vec<String>, store: Arc<Store>) -> RespValue {
-    let _ = store; //Unused for now
 
     if args.is_empty() {
         return RespValue::err("ERR empty command");
     }
+    
+    let cmd = args[0].to_uppercase();
 
-    match args[0].to_uppercase().as_str() {
+    match cmd.as_str() {
         "PING" => {
             if args.len() == 1 {
                 RespValue::SimpleString("PONG".into())
             } else if args.len() == 2 {
                 RespValue::bulk(args[1].clone())
             } else {
-                RespValue::err("ERR wrong number of arguments for 'ping' command")
+                RespValue::err(format!("ERR wrong number of arguments for '{}' command", cmd))
             }
         }
+
+        "GET" => {
+            if args.len() != 2 {
+                return RespValue::err(format!("ERR wrong number of arguments for '{}' command", cmd));
+            }
+
+            match store.get(&args[1]) {
+                Some(value) => RespValue::bulk(value),
+                None => RespValue::null(),
+            }
+        }
+
+        "SET" => {
+            if args.len() != 3 {
+                return RespValue::err(format!("ERR wrong number of arguments for '{}' command", cmd));
+            }
+
+            store.set(args[1].clone(), args[2].clone());
+            RespValue::ok()
+        }
+
+        "DEL" => {
+            if args.len() < 2 {
+                return RespValue::err(format!("ERR wrong number of arguments for '{}' command", cmd));
+            }
+
+            let count = store.del(&args[1..]);
+            RespValue::Integer(count)
+        }
+
+        "EXISTS" => {
+            if args.len() < 2 {
+                return RespValue::err(format!("ERR wrong number of arguments for '{}' command", cmd));
+            }
+
+            let count = store.exists(&args[1..]);
+            RespValue::Integer(count)
+        }
+
+        "KEYS" => {
+            if args.len() > 2 {
+                return RespValue::err(format!("ERR wrong number of arguments for '{}' command", cmd));
+            }
+
+            if args.len() == 2 && args[1] != "*" {
+                return RespValue::err("ERR only '*' pattern is supported for KEYS command");
+            }
+            
+            let keys = store.keys();
+            
+            RespValue::Array(Some(
+                keys.into_iter()
+                    .map(|key| RespValue::bulk(key))
+                    .collect()
+            ))
+        }
+
+
 
         cmd => RespValue::err(format!("ERR unknown command '{}'", cmd)),
     }
--- src/store.rs
+++ src/store.rs
@@ -11,4 +11,41 @@ impl Store {
             data: Mutex::new(HashMap::new()),
         }
     }
+
+    pub fn get(&self, key:&str) -> Option<String> {
+        let data = self.data.lock().unwrap();
+        data.get(key).cloned()
+    }
+
+    pub fn set(&self, key: String, value: String) {
+        let mut data = self.data.lock().unwrap();
+        data.insert(key, value);
+    }
+
+    pub fn del(&self, keys: &[String]) -> i64 {
+        let mut data = self.data.lock().unwrap();
+        let mut count = 0;
+        for key in keys {
+            if data.remove(key).is_some() {
+                count += 1;
+            }
+        }
+        count
+    }
+
+    pub fn exists(&self, keys: &[String]) -> i64 {
+        let data = self.data.lock().unwrap();
+        let mut count = 0;
+        for key in keys {
+            if data.contains_key(key) {
+                count += 1;
+            }
+        }
+        count
+    }
+
+    pub fn keys(&self) -> Vec<String> {
+        let data = self.data.lock().unwrap();
+        data.keys().cloned().collect()
+    }
 }
